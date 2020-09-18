39580495ed5220bb8c99a02c5377f302770ae38b
thiagodt1206 <thi.hobbit@gmail.com>
2020-09-17 23:44:12 -0300

    First controller and Razor pages tests

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Salles Web MVC App from C# Course";
+            ViewData["Aluno"] = "Thiago Dias Teixeira";
 
             return View();
         }
