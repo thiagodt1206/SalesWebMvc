2368907620f3c6b83295cb518d2ddbb327f6b463
thiagodt1206 <thi.hobbit@gmail.com>
2020-09-18 01:06:16 -0300

    CRUD Scaffolding

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,7 +4,6 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using SalesWebMvc.Models.ViewModels;
 using SalesWebMvc.Models;
 
 namespace SalesWebMvc.Controllers
