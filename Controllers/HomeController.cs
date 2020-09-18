89ae56fbf905d2ce8964d505efd5ba7e5dfb45ee
thiagodt1206 <thi.hobbit@gmail.com>
2020-09-18 00:50:48 -0300

    First Model-Controller-View - Department

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models.ViewModels;
 using SalesWebMvc.Models;
 
 namespace SalesWebMvc.Controllers
@@ -17,7 +18,7 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Salles Web MVC App from C# Course";
+            ViewData["Message"] = "Sales Web MVC App from C# Course";
             ViewData["Aluno"] = "Thiago Dias Teixeira";
 
             return View();
