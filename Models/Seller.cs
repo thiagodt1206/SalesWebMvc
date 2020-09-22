42c62177de8a6bc49f89da63155687bccffe588d
thiagodt1206 <thi.hobbit@gmail.com>
2020-09-22 16:21:53 -0300

    Validation

--- Models/Seller.cs
+++ Models/Seller.cs
@@ -9,15 +9,25 @@ namespace SalesWebMvc.Models
     public class Seller
     {
         public int Id { get; set; }
+        [Required(ErrorMessage ="{0} obrigatorio")]
+        [StringLength(60,MinimumLength =3, ErrorMessage ="{0}: tamanho deve ter entre {2} e {1} caracteres")]
         public string Name { get; set; }
         [DataType(DataType.EmailAddress)]
+        [Required(ErrorMessage = "{0} obrigatorio")]
+        [EmailAddress(ErrorMessage ="Insira um e-mail valido")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "{0} obrigatorio")]
         [Display(Name="Birth Date")]
         [DataType(DataType.Date)]
         public DateTime BirthDate { get; set; }
+
         [Display(Name="Base Salary")]
         [DisplayFormat(DataFormatString ="{0:C2}")]
+        [Required(ErrorMessage = "{0} obrigatorio")]
+        [Range(100, 50000,ErrorMessage ="Salario tem que ser entre {1} e {2}")]
         public double BaseSalary { get; set; }
+
         public Department Department { get; set; }
         public int DepartmentId { get; set; }
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
