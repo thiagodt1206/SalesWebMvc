d9e394f62b6203c3486d4e56fbfb48e79ae09f93
thiagodt1206 <thi.hobbit@gmail.com>
2020-09-22 15:24:30 -0300

    App locale, number and date formatting

--- Models/Seller.cs
+++ Models/Seller.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 
 
@@ -9,8 +10,13 @@ namespace SalesWebMvc.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
+        [Display(Name="Birth Date")]
+        [DataType(DataType.Date)]
         public DateTime BirthDate { get; set; }
+        [Display(Name="Base Salary")]
+        [DisplayFormat(DataFormatString ="{0:C2}")]
         public double BaseSalary { get; set; }
         public Department Department { get; set; }
         public int DepartmentId { get; set; }
